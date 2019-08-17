0b2d87f937147d57f23b149e8bd554843787dacf
Clare <chblackburne@gmail.com>
2019-08-17 18:43:30 +0100

    dummy text

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,9 +2,18 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import SEO from "../components/seo"
+import styled from "@emotion/styled"
 import { css } from "@emotion/core"
 
 import Header from '../components/header'
+
+const Text = styled.p`
+  line-height: 1.5rem;
+  ::first-letter {
+    font-size: 200%;
+    font-weight: bold;
+    color: #8A2BE2;
+`
 
 const About = ({ data }) => {
   console.log('data', data)
@@ -17,9 +26,13 @@ const About = ({ data }) => {
       alt="Sketchbook"
       css={css`
         max-width: 400px;
+        margin: 50px 0;
         box-shadow: 0px 3px 15px rgba(0,0,0,0.3);
         border-radius: 5px;
       `} />
+    <Text>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Congue nisi vitae suscipit tellus mauris a diam maecenas sed. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Massa id neque aliquam vestibulum morbi. Malesuada proin libero nunc consequat interdum. At imperdiet dui accumsan sit amet nulla. Aliquam ut porttitor leo a diam sollicitudin tempor. Tortor id aliquet lectus proin nibh. Sit amet dictum sit amet justo donec enim diam. Nunc sed augue lacus viverra. Nunc lobortis mattis aliquam faucibus purus in. Euismod lacinia at quis risus sed vulputate odio ut enim. Amet dictum sit amet justo. Eu tincidunt tortor aliquam nulla. Quis auctor elit sed vulputate mi sit amet. Bibendum enim facilisis gravida neque. Quis viverra nibh cras pulvinar mattis nunc. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus.
+    </Text>
   </React.Fragment>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,29 @@
 import React from "react"
 import Img from "gatsby-image"
 import SEO from "../components/seo"
+import styled from "@emotion/styled"
 import { css } from "@emotion/core"
 
+const Text = styled.p`
+  line-height: 1.5rem;
+  ::first-letter {
+    font-size: 200%;
+    font-weight: bold;
+    color: #8A2BE2;
+  }
+`
+const TextBox = styled.div`
+  width: 80%;
+`
 const IndexPage = ({ data }) => (
   <div css={css`
     height: 100%;
     `}>
     <SEO title="Home" keywords={[`barns`, `Yorkshire`, `Dales`]} />
     <h1>Welcome</h1>
+    <h3 css={css`
+      color: grey;
+    `}>Subtitle about the website goes here</h3>
     <Img
       fluid={data.image.childImageSharp.fluid}
       alt="Dales Barn"
@@ -16,7 +31,11 @@ const IndexPage = ({ data }) => (
         max-width: 400px;
         box-shadow: 0px 3px 15px rgba(0,0,0,0.3);
         border-radius: 5px;
+        margin: 50px 0;
       `} />
+    <TextBox>
+      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Congue nisi vitae suscipit tellus mauris a diam maecenas sed. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Massa id neque aliquam vestibulum morbi. Malesuada proin libero nunc consequat interdum. At imperdiet dui accumsan sit amet nulla. Aliquam ut porttitor leo a diam sollicitudin tempor. Tortor id aliquet lectus proin nibh. Sit amet dictum sit amet justo donec enim diam. Nunc sed augue lacus viverra. Nunc lobortis mattis aliquam faucibus purus in. Euismod lacinia at quis risus sed vulputate odio ut enim. Amet dictum sit amet justo. Eu tincidunt tortor aliquam nulla. Quis auctor elit sed vulputate mi sit amet. Bibendum enim facilisis gravida neque. Quis viverra nibh cras pulvinar mattis nunc. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus.</Text>
+    </TextBox>
   </div>
 )
 
