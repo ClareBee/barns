3522e1f6df1227b15a1ac10111484f6f92c87a22
Clare <chblackburne@gmail.com>
2019-11-03 12:51:32 +0000

    content for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -34,15 +34,32 @@ const Section = styled.section`
 `
 const Text = styled.p`
   ::first-letter {
-    font-size: 150%;
+    font-size: 2rem;
     font-weight: bold;
     color: ${props => props.theme.colors.mountbattenPink};
+`
+const QuotationMark = styled.span`
+  font-size: 2rem;
+  line-height: 1.5rem;
+  font-weight: bold;
+  color: ${props => props.theme.colors.mountbattenPink};
+`
+const Divider = styled.hr`
+  width: 90%;
+  height: 5px;
+  opacity: 0.7;
+  background-color: ${props => props.theme.colors.mountbattenPink};
+  @media (max-width: ${props => props.theme.breakpoints.md}) {
+    width: 100%;
+  }
 `
 
 const About = props => (
   <React.Fragment>
     <SEO title="About" />
     <Header title="About" id="top"/>
+    <Divider />
+
     <Links>
       <li>
         <a href="#artist">The Artist</a>/
@@ -54,6 +71,8 @@ const About = props => (
         <a href="#resources">Resources</a>
       </li>
     </Links>
+    <Divider />
+
     <img
       src={sketchbook}
       alt="Sketchbook"
@@ -69,11 +88,12 @@ const About = props => (
     />
     <Section id="artist">
       <h2>Margaret Blackburne - The Artist</h2>
-      <Text>
+      <p>
+        <QuotationMark>&#8220;</QuotationMark>
         Born and brought up on a Dales farm, I have had a life-long interest in
         painting and have been especially inspired by landscapes and the natural
         beauty of the surrounding area.
-      </Text>
+      </p>
       <p>
         Over the years, I have painted many of the barns in the Dales and have
         become increasingly concerned about the disappearance and decay of many
@@ -84,24 +104,43 @@ const About = props => (
         This website is intended as an ongoing project to share this collection
         with a wider audience, so that others may also witness this changing
         history.
+        <QuotationMark>&#8221;</QuotationMark>
       </p>
       <a href="#top">Top</a>
     </Section>
+
+    <Divider />
+
     <Section>
       <MyMap allBarns={props.data.barns} />
     </Section>
+    <Divider />
     <Section id="project">
       <h2>Disappearing Barns - The Yorkshire Dales</h2>
       <Text>
-        There are roughly 4,500 barns dotted around the Yorkshire Dales, most of
-        them built between the 17th and 18th centuries.
+        There are roughly 4,500 barns dotted around the Yorkshire Dales, most them built between the 17th and 18th centuries.
       </Text>
       <p>
-        Dales barns - also known as laithes - are unusual in being located far
-        from other farm buildings.
+         Field barns - also known as laithes - are unusual in being located far from other farm buildings, making them a distinctive feature of the Dales landscape. Darkened slate roofs in the rain, walls bleached light in sunshine, their very presence seems attuned with the weather, somewhere mid-way between man-made and organic.
+      </p>
+      <p>
+        With different dales comes different stones - sandstone, limestone, gritstone - while the buildings themselves still bear the marks of their construction - ‘putlog’ holes where scaffolding poles would have been placed, ‘tiestones’ running through the entire width of a wall and the loadbearing ‘quoins’ at the corners.
+      </p>
+      <p>
+        The shadowed interiors hosted cow in ‘shippen’, hay stored above in the ‘baulks’ and served up in ‘foddergang’, harking back to a forgotten vocabulary and a dwindling skillset.
+      </p>
+      <p>
+        Margaret Blackburne, the artist, recalls that, as a child, it was her and her siblings’ job to trample down the hay for storage - which also provided temporary bedding for passing tramps who would seek shelter in the barns for the night and then venture down to the family farm for hot mugs of tea.
+      </p>
+      <p>
+        While some barns are still in active use, many others have fallen into disrepair, crumbling a little further with each year that passes. Still others have been converted into houses, raising some tensions between the desire to maintain the traditional farming practices of the Dales and the need to provide affordable housing for its inhabitants.
+      </p>
+      <p>
+        This project aims to capture these barns at various points in time. Some of those featured have fallen down completely since they were painted - and, while there are efforts by the Yorkshire Dales National Park to fund their preservation, it is unfortunately already too late for many.
       </p>
       <a href="#top">Top</a>
     </Section>
+    <Divider />
     <Section id="resources">
       <Resources />
       <a href="#top">Top</a>
